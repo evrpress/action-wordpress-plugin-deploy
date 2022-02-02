b3156c464344829c1f83637de5acf73c0115abe9
Xaver <xaver@everpress.co>
2022-02-02 17:35:24 +0100

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -125,8 +125,9 @@ fi
 if [[ -d "$GITHUB_WORKSPACE/$ASSETS_DIR/" ]]; then
 
 	echo
-	echo ".........................................."
-	echo "Preparing assets"
+
+	ls -a $GITHUB_WORKSPACE/$ASSETS_DIR
+	echo "➤ Preparing assets..."
 	convert -resize 1544x500 $GITHUB_WORKSPACE/$ASSETS_DIR/banner.png $GITHUB_WORKSPACE/$ASSETS_DIR/banner-1544x500.png
 	convert -resize 772x250 $GITHUB_WORKSPACE/$ASSETS_DIR/banner.png $GITHUB_WORKSPACE/$ASSETS_DIR/banner-772x250.png
 	convert -resize 256x256 $GITHUB_WORKSPACE/$ASSETS_DIR/icon.png $GITHUB_WORKSPACE/$ASSETS_DIR/icon-256x256.png
